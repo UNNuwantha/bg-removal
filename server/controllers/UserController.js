7b18f8d301458ee065edc1fa122a4b6877c423b7
UNNuwantha <www.nadun50@gmail.com>
2026-04-02 14:16:32 +0530

    Added Credit Features

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -64,4 +64,30 @@ const clerkWebhooks = async (req,res) => {
 }
 
 
-export {clerkWebhooks}
+
+
+//API Controller function to get user available cedits data
+const userCredits = async (req,res) => {
+    try {
+        
+      const clerkId = req.clerkId
+
+      const userData = await userModel.findOne({clerkId})
+
+      if(!userData){
+        return res.json({success:false, message:'User not found'})
+      }
+
+      res.json({success:true, credits:userData.creditBalance})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+
+
+export {clerkWebhooks, userCredits }
