8bae3f3606e18d5f9a4acb35a2221f31968e220e
UNNuwantha <www.nadun50@gmail.com>
2026-04-04 07:02:51 +0530

    Testing and Developing ImageUploder

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,18 +1,24 @@
-import { useAuth } from "@clerk/react";
+import { useAuth, useClerk, useUser } from "@clerk/react";
 import { useState } from "react";
 import { createContext } from "react";
 import axios from 'axios'
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
  export const AppContext = createContext()
 
  const AppContextProvider = (props) => {
 
     const [credit, setCredit] = useState(false)
+    const [image, setImage] = useState(false)
+    const [resultImage, setResultImage] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const navigate = useNavigate()
 
     const { getToken } = useAuth() 
+    const { isSignedIn } = useUser()
+    const { openSignIn } = useClerk() 
 
      const loadCreditsData = async () => {
         try {
@@ -27,10 +33,32 @@ import { toast } from "react-toastify";
             toast.error(error.message)
         }
      }
+     const removeBg = async (image) => {
+        try {
+
+           if(!isSignedIn) {
+            return openSignIn()
+           }
+
+           setImage(image)
+           setResultImage(false)
+
+           navigate('/result')
+            
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+     }
+
+
     const value = {
        credit, setCredit,
        loadCreditsData,
-       backendUrl
+       backendUrl,
+       image, setImage,
+       removeBg
     }
 
     return(
